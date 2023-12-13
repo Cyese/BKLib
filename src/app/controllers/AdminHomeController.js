4b3a8d70467499bce5ab5fa159a213d48b3839f9
Cyese <87186561+Cyese@users.noreply.github.com>
2023-12-13 10:48:02 +0700

    haha

--- src/app/controllers/AdminHomeController.js
+++ src/app/controllers/AdminHomeController.js
@@ -1,9 +1,7 @@
-const { render } = require('node-sass')
-const { multipleToObject } = require('../../util/mongoose')
-const account=require('../models/old/accountdb')
+// const account=require('../models/old/accountdb')
 const Receipt = require('../models/receipt');
-const book=require('../models/old/book')
-const Library = require('./Library')
+// const book=require('../models/old/book')
+// const Library = require('./Library')
 class AdminHomeController{
     index(req, res){
         res.render('adminhome', {
@@ -11,57 +9,57 @@ class AdminHomeController{
         })
     }
 
-    accountManage(req, res, next){
-       account.find({})
-            .then(accounts => {
-                res.render('accountManage', {
-                    layout: 'admin',
-                    accounts: multipleToObject(accounts)
-                })
-            })
-            .catch(next)
-    }
+    // accountManage(req, res, next){
+    //    account.find({})
+    //         .then(accounts => {
+    //             res.render('accountManage', {
+    //                 layout: 'admin',
+    //                 accounts: multipleToObject(accounts)
+    //             })
+    //         })
+    //         .catch(next)
+    // }
 
-    destroy(req, res, next){
-        account.deleteOne({_id: req.params.id})
-            .then(() => res.redirect('back'))
-            .catch(next)
-    }
+    // destroy(req, res, next){
+    //     account.deleteOne({_id: req.params.id})
+    //         .then(() => res.redirect('back'))
+    //         .catch(next)
+    // }
 
-    bookManage(req, res, next){
-        book.find({})
-            .then(books => {
-                res.render('bookManage', {
-                    layout: 'admin',
-                    books: multipleToObject(books)
-                })
-            })
-            .catch(next)
-    }
-    bookModify(req, res, next){
-        book.deleteOne({_id: req.params.id})
-            .then(() => res.redirect('back'))
-            .catch(next)
-    }
-    async bookModify(req, res, next){
-        book.deleteOne({_id: req.params.id})
-            .then(() => res.redirect('back'))
-            .catch(next)
-    }
+    // bookManage(req, res, next){
+    //     book.find({})
+    //         .then(books => {
+    //             res.render('bookManage', {
+    //                 layout: 'admin',
+    //                 books: multipleToObject(books)
+    //             })
+    //         })
+    //         .catch(next)
+    // }
+    // bookModify(req, res, next){
+    //     book.deleteOne({_id: req.params.id})
+    //         .then(() => res.redirect('back'))
+    //         .catch(next)
+    // }
+    // async bookModify(req, res, next){
+    //     book.deleteOne({_id: req.params.id})
+    //         .then(() => res.redirect('back'))
+    //         .catch(next)
+    // }
 
-    async addBook(req,res){
-        const Book = new book({
-            title : req.query.title, 
-            author:  req.query.author,  
-            publishYear : req.query.publishYear, 
-            type :req.query.type,
-            quantity : 1,
-            available :1
-        })
-        await Book.save().then(
-            res.redirect('/adminhome/book')
-        )
-    }
+    // async addBook(req,res){
+    //     const Book = new book({
+    //         title : req.query.title, 
+    //         author:  req.query.author,  
+    //         publishYear : req.query.publishYear, 
+    //         type :req.query.type,
+    //         quantity : 1,
+    //         available :1
+    //     })
+    //     await Book.save().then(
+    //         res.redirect('/adminhome/book')
+    //     )
+    // }
 
     async getReceipt(req, res, next){
         const result = await Receipt.loadReceipt();
